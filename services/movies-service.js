7fbec019ad8aa4c1517cf3c5e0b0cb9c85fde494
ycervantes <cervantes.y@northeastern.edu>
2021-11-11 23:12:25 -0800

    first commit

---
+++ services/movies-service.js
@@ -0,0 +1,36 @@
+let movies = require("./movies.json");
+
+module.exports = (app) => {
+    const getAllMovies = (req, res) =>{
+        res.json(movies);
+    }
+    const deleteMovie = (req, res) => {
+        const id = req.params['mid'];
+        movies = movies.filter(m => m._id !== id);
+        res.json(movies);
+    };
+    const createMovie = (req, res) => {
+        const movie = req.body;
+        movies = [...movies, movie];
+        res.json(movies);
+    }
+    const saveMovie = (req, res) => {
+        const newMovie = req.body;
+        const movieId = req.params['mid'];
+        movies = movies.map(movie =>
+            movie._id === movieId ? newMovie : movie);
+        res.json(movies);
+    }
+    app.put('/api/movies/:mid', saveMovie);
+    app.get('/api/movies', getAllMovies);
+    app.delete('/api/movies/:mid', deleteMovie);
+    app.post('/api/movies', createMovie);
+
+
+
+};
+
+
+
+
+
